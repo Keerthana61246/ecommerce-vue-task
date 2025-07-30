2b18ad793df3cdb25a5c8f841ee367dfb56f5193
GANGARAJU KEERTHANA <keerthanagangaraju35@gmail.com>
2025-07-30 09:59:30 +0530

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-app :theme="$store.state.theme">
+    <v-app-bar app>
+      <v-toolbar-title>E-Commerce</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn @click="$store.commit('toggleTheme')">Toggle Theme</v-btn>
+    </v-app-bar>
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
+</template>
---
+++ main.js
@@ -0,0 +1,8 @@
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import vuetify from './plugins/vuetify';
+
+const app = createApp(App);
+app.use(router).use(store).use(vuetify).mount('#app');
